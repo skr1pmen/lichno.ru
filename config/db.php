f7fc434211fa96403398c1210e02fd0548d28d16
Danil <danil@flower.it-finans.ru>
2024-09-26 18:07:02 +0400

    config/db.php env support

--- config/db.php
+++ config/db.php
@@ -1,10 +1,15 @@
 <?php
 
+$DB_HOST = getenv('DATABASE_HOST') ?: 'localhost';
+$DB_NAME = getenv('DATABASE_NAME') ?: 'lichno';
+$DB_USER = getenv("DATABASE_USER") ?: 'postgres';
+$DB_PASSWORD = getenv("DATABASE_PASSWORD") ?: '';
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=lichno',
-    'username' => 'postgres',
-    'password' => '',
+    'dsn' => "pgsql:host=$DB_HOST;dbname=$DB_NAME",
+    'username' => $DB_USER,
+    'password' => $DB_PASSWORD,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
